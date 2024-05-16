ddc462c977bc9c25855ecaa431938748d0b92aaf
davidjonathan01 <davidfernandezdavila01@gmail.com>
2024-05-16 06:22:11 -0500

    Detalles finales

--- models/eval_ansiedad.py
+++ models/eval_ansiedad.py
@@ -2,12 +2,12 @@ from utils.db import db
 from models.test_ansiedad import TestAnsiedad
 class EvalAnsiedad(db.Model):
     __tablename__ = 'eval_ansiedad'
-<<<<<<< HEAD
+
     id_evaluacion = db.Column(db.Integer, primary_key=True)
     id_test_ansiedad = db.Column(db.Integer, db.ForeignKey('test_ansiedad.id_test'))
     respuestas = db.Column(db.String(255))
     fecha_evaluacion = db.Column(db.Date)
-=======
+    
     idEvaluacion = db.Column(db.Integer, primary_key=True)
     idTestAnsiedad = db.Column(db.Integer, db.ForeignKey('test_ansiedad.idTest'))
     respuestas = db.Column(db.String(100000))
