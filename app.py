586a6119348ad4a4d6bf57ebd93f0d37dbd080d3
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-06-06 03:53:02 -0500

    version 2.1

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from utils.db import db
 #from services.contact import contacts
-from services.estudiante import estudiante_routes
+'''from services.estudiante import estudiante_routes
 from services.exp_psi_estudiante import exp_psi_estudiante_routes
 from services.test_ansiedad import test_ansiedad_routes
 from services.eval_ansiedad import eval_ansiedad_routes
-from services.hist_eval_ansiedad import hist_eval_ansiedad_routes
-from functions.realizar_test_ansiedad import cus
+from services.hist_eval_ansiedad import hist_eval_ansiedad_routes'''
+from services.genero import genero_routes
+#from functions.realizar_test_ansiedad import cus
 
 from flask_sqlalchemy import SQLAlchemy
 from config import DATABASE_CONNECTION
@@ -18,12 +19,13 @@ app.config['SQLALCHEMY_DATABASE_URI']=DATABASE_CONNECTION
 #SQLAlchemy(app)
 db.init_app(app)
 
-app.register_blueprint(estudiante_routes)
+'''app.register_blueprint(estudiante_routes)
 app.register_blueprint(exp_psi_estudiante_routes)
 app.register_blueprint(test_ansiedad_routes)
 app.register_blueprint(eval_ansiedad_routes)
 app.register_blueprint(hist_eval_ansiedad_routes)
-app.register_blueprint(cus, url_prefix='/cus')
+app.register_blueprint(cus, url_prefix='/cus')'''
+app.register_blueprint(genero_routes, url_prefix='/genero_routes')
 
 
 
